f97232f0f19ef165a7e152daceedb66a9cb2c91b
TateMyers <tateymyers@gmail.com>
2026-04-03 09:46:09 -0600

    hopefully by changing the images file source this finally works

--- game/script.js
+++ game/script.js
@@ -18,7 +18,7 @@ function drawGrid() {
       // Check if player is here
       if (r === player.row && c === player.col) {
         const img = document.createElement("img");
-        img.src = "game/assets/monsterDude.png"; // replace with your image
+        img.src = "assets/monsterDude.png"; // replace with your image
         img.classList.add("player");
         cell.appendChild(img);
       }
